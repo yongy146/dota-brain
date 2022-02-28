d9efd15cb1138a5de6426e6ecdf253af6e783610
Michel <37207948+roserens@users.noreply.github.com>
2022-02-28 07:33:09 +0100

    Update

--- messages.ts
+++ messages.ts
@@ -2,8 +2,6 @@
 
    This document contains all the messages played by Dota Coach.
 
-HELLO, I'M HERRE
-
   Questions and comments:
     // DayTime
     Is it worth adding it for 0min switch to daytime? -> Recent patch might make it worth it
