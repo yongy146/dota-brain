ddcb1a3871525d93e4e591a93e6be2740994da38
dota-coach <info@dota-coach.com>
2023-01-19 13:55:14 +0100

    Update

--- monitorSizes/monitorMeasurements.ts
+++ monitorSizes/monitorMeasurements.ts
@@ -1,60 +1,57 @@
 /**
- * This class provides information on the screens in Dota 2 for the app to be able to properly position windows
+ * This module provides information on the screens in Dota 2 for the app to be able to properly position windows
  *
- * (C) Dota Coach, 2021
+ * (C) Dota Coach, 2023
  */
-import { IMonitorMeasurements, IMonitorReuse, measurements } from "./measurements";
+import {
+  IMonitorMeasurements,
+  IMonitorMeasurementsOverwolf,
+  IMonitorReuse,
+  measurements,
+} from "./measurements";
+import * as Logger from "../../../submodules/utilities/log";
 
-export class MonitorMeasurements {
-  public readonly width: number;
-  public readonly height: number;
-  public measurements: IMonitorMeasurements;
-  public readonly customization: "piggy backed" | "customized" | "not configured";
+/**
+ * The function information on Dota 2 screens. This information can be used to position elements in the game.
+ *
+ * If there is not details availalbe for proivded screenSize, the it returns information for the default screen size, which is 1920x1080
+ *
+ * @param screenSize Screen size, e.g. 1920x1080
+ * @returns
+ */
+export function getMonitorMeasurements(width: number, height: number): IMonitorMeasurements {
+  let result: Partial<IMonitorMeasurements> = {
+    width,
+    height,
+  };
 
-  get isCustomized(): boolean {
-    return this.customization !== "not configured";
+  const screenSize = `${width}x${height}`;
+  if (measurements[screenSize] === undefined) {
+    result.overwolf = <IMonitorMeasurementsOverwolf>measurements["1920x1080"];
+    result.isCustomized = false;
+    result.customization = "not configured";
+  } else if ((<IMonitorReuse>measurements[screenSize]).reuse !== undefined) {
+    result.overwolf = <IMonitorMeasurementsOverwolf>(
+      measurements[(<IMonitorReuse>measurements[screenSize]).reuse]
+    );
+    result.isCustomized = true;
+    result.customization = "piggy backed";
+  } else {
+    result.overwolf = <IMonitorMeasurementsOverwolf>measurements[screenSize];
+    result.isCustomized = true;
+    result.customization = "customized";
   }
 
-  get fontSize(): number {
-    return this.measurements.fontSize / window.devicePixelRatio;
-  }
-  get fontSizeSmall(): number {
-    return this.fontSize * 0.8;
-  }
+  // Addjust html sizing for device-pixel-ratio
+  const fontSize = result.overwolf.fontSize / window.devicePixelRatio;
+  result.html = {
+    fontSize,
+    fontSizeSmall: fontSize * 0.8,
+    inGame: {
+      heroesWidth: result.overwolf.inGame.heroesWidth / window.devicePixelRatio,
+      heroesHeight: result.overwolf.inGame.heroesHeight / window.devicePixelRatio,
+    },
+  };
 
-  /**
-   * The function information on Dota 2 screens. This information can be used to position elements in the game.
-   *
-   * If there is not details availalbe for proivded screenSize, the it returns information for the default screen size, which is 1920x1080
-   *
-   * @param screenSize Screen size, e.g. 1920x1080
-   * @returns
-   */
-  public constructor(width: number, height: number) {
-    this.width = width;
-    this.height = height;
-
-    const screenSize = `${width}x${height}`;
-    if (measurements[screenSize] === undefined) {
-      this.measurements = <IMonitorMeasurements>measurements["1920x1080"];
-      this.customization = "not configured";
-      this.measurements.isCustomized = false;
-    } else if ((<IMonitorReuse>measurements[screenSize]).reuse !== undefined) {
-      this.measurements = <IMonitorMeasurements>(
-        measurements[(<IMonitorReuse>measurements[screenSize]).reuse]
-      );
-      this.customization = "piggy backed";
-      this.measurements.isCustomized = true;
-    } else {
-      this.measurements = <IMonitorMeasurements>measurements[screenSize];
-      this.customization = "customized";
-      this.measurements.isCustomized = true;
-    }
-
-    this.measurements = JSON.parse(JSON.stringify(this.measurements));
-
-    this.measurements.width = width;
-    this.measurements.height = height;
-    this.measurements.fontSizeSmall = this.measurements.fontSize * 0.8;
-  }
+  return result as IMonitorMeasurements;
 }
