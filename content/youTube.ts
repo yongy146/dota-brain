a043153951fca964bde366fd5366e55e6356fe06
dota-coach <info@dota-coach.com>
2023-11-20 14:37:47 +0100

    Update

--- content/youTube.ts
+++ content/youTube.ts
@@ -3,14 +3,12 @@
  *
  */
 
-import { IYouTubeVideo } from "./itemContent";
-
 // https://www.youtube.com/watch?v=_HBcbAxlcdY
 
-export const youTubeVideos: Record<string, IYouTubeVideo | IYouTubeVideo[]> = {
-  items: {
+export const youTubeVideos: Record<string, string[]> = {
+  items: [
     // Date: 14.8.2020
     // Searched: 25.10.2023
-    code: "PZr8AxyQ8E0",
-  },
+    "PZr8AxyQ8E0",
+  ],
 };
