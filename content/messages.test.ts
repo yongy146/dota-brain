21fb2650dc08fddc755d3612a769d3b60ae4f4bb
dota-coach <info@dota-coach.com>
2023-11-21 11:35:18 +0100

    Update

--- content/messages.test.ts
+++ content/messages.test.ts
@@ -2,18 +2,32 @@
  * npx jest messages.test.ts
  *
  */
-import { dotaCoachMessages, getOwnHeroMessages } from "./messages";
-import dota2Heroes from "../submodules/gameData/out/dota2Heroes.json";
+import { DOTA_COACH_GUIDE_ROLE } from "../utilities/playerRoles";
+import {
+  dotaCoachMessages,
+  getOwnHeroMessages,
+  getOwnHeroMessagesForRoles,
+} from "./messages";
+import dota2Heroes from "@gameData/out/dota2Heroes.json";
 
 test("messages-getOwnHeroMessages", () => {
-  // Checks that all heroes has own messages
+  // Checks that all heroes have own messages
   for (const npcName of Object.keys(dota2Heroes)) {
     const msgs = getOwnHeroMessages(npcName.replace("npc_dota_hero_", ""));
     expect(msgs.length > 0 || npcName).toBe(true);
   }
 });
 
-test("messages-messages", () => {
+test("messages-getOwnHeroMessagesForRoles", () => {
+  // Checks that all heroes have own messages
+  const msgs = getOwnHeroMessagesForRoles("npc_dota_hero_rubick", [
+    DOTA_COACH_GUIDE_ROLE.SUPPORT,
+  ]);
+  console.log(`msgs: `, msgs);
+  expect(msgs.length > 0).toBe(true);
+});
+
+test("messages-getOwnHeroMessagesForRoles", () => {
   // Checks that all heroes in messages.ts exists
   for (const message of dotaCoachMessages) {
     if (message.npcHeroName) {
