a66db9c3c5a3d29f677ddd753fab949b8d03f8da
dota-coach <info@dota-coach.com>
2024-02-08 15:24:28 +0100

    Update

--- scripts/counterItemTooltips.test.ts
+++ scripts/counterItemTooltips.test.ts
@@ -7,7 +7,7 @@
 import { IntlShape } from "react-intl";
 import { i18nLoader } from "@utilities/i18n/i18nLoader";
 import { heroBuilds } from "../content/heroBuilds";
-import { getItemTooltip } from "../access/heroBuilds";
+import { getCounterItemTooltip, getItemTooltip } from "../access/heroBuilds";
 
 let intl: IntlShape | undefined;
 beforeAll(async () => {
@@ -31,34 +31,31 @@ test("counterItemTooltips", () => {
   const foundTooltips = [];
   const missingTooltips = [];
   for (const [npcShortName, heroContent] of Object.entries(heroBuilds)) {
-      const counterItems = [
-        ...heroContent.counter_items.laning_phase.all,
-        ...heroContent.counter_items.laning_phase.support,
-        ...heroContent.counter_items.laning_phase.core,
-        ...heroContent.counter_items.mid_game.all,
-        ...heroContent.counter_items.mid_game.support,
-        ...heroContent.counter_items.mid_game.core,
-        ...heroContent.counter_items.late_game.all,
-        ...heroContent.counter_items.late_game.support,
-        ...heroContent.counter_items.late_game.core,
-      ];
-      for (const counterItem of counterItems) {
-        getCounterItemTooltip
+    const counterItems = [
+      ...heroContent.counter_items.laning_phase.all,
+      ...heroContent.counter_items.laning_phase.support,
+      ...heroContent.counter_items.laning_phase.core,
+      ...heroContent.counter_items.mid_game.all,
+      ...heroContent.counter_items.mid_game.support,
+      ...heroContent.counter_items.mid_game.core,
+      ...heroContent.counter_items.late_game.all,
+      ...heroContent.counter_items.late_game.support,
+      ...heroContent.counter_items.late_game.core,
+    ];
+    for (const counterItem of counterItems) {
+      const tooltip = getCounterItemTooltip(npcShortName, counterItem, intl);
 
-        const tooltip = getItemTooltip(npcShortName, i, coreItem, intl!);
-        if (!tooltip) {
-          missingTooltips.push(
-            `${npcShortName}, build ${i}, item: ${coreItem}`
-          );
-        } else {
-          foundTooltips.push(`${npcShortName}, build ${i}, item: ${coreItem}`);
-        }
+      if (!tooltip) {
+        missingTooltips.push(`${npcShortName}, ${counterItem}`);
+      } else {
+        foundTooltips.push(`${npcShortName}, ${counterItem}`);
       }
     }
   }
 
   //console.log(`Found tooltips:\n`, JSON.stringify(foundTooltips, null, 2));
-  console.log(`Missing tooltips:\n`, JSON.stringify(missingTooltips, null, 2));
+  const result = [...new Set(missingTooltips)];
+  console.log(`Missing tooltips:\n`, JSON.stringify(result, null, 2));
 
-  expect(missingTooltips.length).toBe(0);
+  expect(result.length).toBe(0);
 });
