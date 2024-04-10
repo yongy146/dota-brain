0f52ac6b65985f44945669394bdaae975c8fce3d
dota-coach <info@dota-coach.com>
2024-04-10 08:32:55 +0200

    Update

--- content/powerSpikes.ts
+++ content/powerSpikes.ts
@@ -10,16 +10,16 @@
  *   - Mid game
  *   - Late game
  *   - Very late game
- * 
- * The power spike values are between 0 and 3 and 
+ *
+ * The power spike values are between 0 and 3 and
  * interpreted as follows:
  *  - Low:    0 - 1 (use 0.5 for average low)
  *  - Medium: 1 - 2 (use 1.5 for average medium; note that 1 is considered medium)
  *  - High:  2 - 3 (use 2.5 for average high; note that 2 is considered high)
- * 
+ *
  * Further details on the game phases can be found here:
  *   - https://dotacoach.atlassian.net/wiki/spaces/D2/pages/8093725/Definitions
- * 
+ *
  */
 export const powerSpikes: Record<string, number[]> = {
   abaddon: [0.5, 1.5, 2.5, 2.5],
@@ -30,7 +30,7 @@ export const powerSpikes: Record<string, number[]> = {
   axe: [0.5, 1.5, 2.5, 2.5],
   bane: [0.5, 1.5, 2.5, 2.5],
   batrider: [0.5, 1.5, 2.5, 2.5],
-  beastmaster: [0.5, 1.5, 2.5, 2.5],,
+  beastmaster: [0.5, 1.5, 2.5, 2.5],
   bloodseeker: [0.5, 1.5, 2.5, 2.5],
   bounty_hunter: [0.5, 1.5, 2.5, 2.5],
   brewmaster: [0.5, 1.5, 2.5, 2.5],
